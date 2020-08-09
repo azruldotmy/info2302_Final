c51c2f513fd8e066b63e1ccfed702e8c8a37f118
WAN MUHAMMAD IZZAT BIN WAN ABD GHAPAR <ijat191999@gmail.com>
2020-08-09 23:25:35 +0800

    update upsr.js

--- project/assets/js/upsr.js
+++ project/assets/js/upsr.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
-  $.getJSON(".vscode/upsr.json",function(data){
-    $.each(data, function(i, upsr){
-      $("#chooseSubject").append(
+$(document).ready(function () {
+  $.getJSON(".vscode/upsr.json", function (data) {
+    $.each(data, function (i, upsr) {
+      $("#chooseSubject").append(`
         <div class= "col-12 m-2">
         <a href="${upsr.path}" class= "btn btn-secondary">
         ${upsr.name}</a>
         </div>
-      )
+      `)
     })
   })
 })
