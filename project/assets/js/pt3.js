6100f2cca6db46b8f090beb5e7e03a710dab20d0
WAN MUHAMMAD IZZAT BIN WAN ABD GHAPAR <ijat191999@gmail.com>
2020-08-09 23:05:31 +0800

    update pt3.js

--- project/assets/js/pt3.js
+++ project/assets/js/pt3.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
-  $.getJSON(".vscode/upsr.json",function(data){
-    $.each(data, function(i, upsr){
-      $("#chooseSubject").append(
+$(document).ready(function () {
+  $.getJSON(".vscode/upsr.json", function (data) {
+    $.each(data, function (i, upsr) {
+      $("#chooseSubject").append(`
         <div class= "col-12 m-2">
         <a href="${upsr.path}" class= "btn btn-secondary">
         ${pt3.name}</a>
         </div>
-      )
+      `)
     })
   })
 })
